87e3bd9070e2f911627c95111b4730d22c7d710a
Mathys <mathystheolade973@gmail.com>
2022-09-16 19:01:13 +0200

    feat: give choice to alter default events or add events

--- libs/events/events.ts
+++ libs/events/events.ts
@@ -12,10 +12,13 @@ export class Events {
 
   constructor(app: EasyApi) {
     this.app = app;
-    this.loadEvents();
   }
 
-  private loadEvents() {
+  /**
+   * It takes an array of objects, each object has a name and a callback function, and it loops through the array and adds
+   * an event listener to the event emitter for each object
+   */
+  public loadEvents() {
     this.events.map(event => {
       this._eventEmitter.on(event.name, () => {
         event.log
@@ -24,6 +27,28 @@ export class Events {
         event.cb();
       });
     });
+  }
+
+  /**
+   * The addListener function takes an event object as an argument and adds it to the event emitter
+   * You can also alter the default event before initializing  it
+   * @param {IEvents} event - IEvents - this is the interface that we created earlier.
+   */
+  public addListener(event: IEvents): void {
+    this._eventEmitter.on(event.name, () => {
+      event.log
+        ? this.app.logger.info(`${event.name} events loaded`)
+        : undefined;
+      event.cb();
+    });
+  }
+
+  /**
+   * The addEvent function takes an event object as a parameter and adds it to the events array
+   * @param {IEvents} event - IEvents - This is the event that we want to add to the array.
+   */
+  public addEvent(event: IEvents): void {
+    this._events.push(event);
   }
 
   get events(): Array<IEvents> {
