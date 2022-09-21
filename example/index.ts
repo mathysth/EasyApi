ca0cde83185ce9d0ebebfad6a17185e90ef64b0c
Mathys <mathystheolade973@gmail.com>
2022-09-21 19:19:57 +0200

    add autoload plugin

--- example/index.ts
+++ example/index.ts
@@ -1,51 +1,74 @@
 import EasyApi from '../app';
-import { FastifyPluginCallback } from 'fastify';
+import { FastifyInstance, FastifyPluginCallback } from 'fastify';
+import recaptchaPl from 'fastify-recaptcha';
+import PATH from 'path';
 
+const dirname = PATH.dirname(__filename);
 const api: EasyApi = new EasyApi({
   env: 'development',
-  auth: { secret: 'bonjour', opts: { maxAge: '3 days' } }
+  auth: { secret: 'bonjour', opts: { maxAge: '3 days' } },
+  // Register default fastify plugin from api
+  // If false you can use api.register() to register them
+  defaultPlugin: true
 });
-const server = api.getServer();
+const server: FastifyInstance = api.getServer();
 
 // Creating custom event
 // If added before: api.events.loadEvents() the event will automatically be injected
 // Else you should call addListener() function from api.events
 api.events.addEvent({
   name: 'hello',
-  log: false,
   cb: () => {
     api.logger.info('Hello world');
   }
 });
 
-// Custom route using Fastify
-const app: FastifyPluginCallback = (server, options, done) => {
-  api.logger.info('Route loaded');
-
-  server.get('/ping', (req, res) => {
-    res.send('pong').status(200);
-  });
-  done();
+//Register custom plugin
+const recaptchaPlugin = {
+  name: 'fastifyRecaptcha',
+  plugin: recaptchaPl,
+  opts: {}
 };
+api.register(recaptchaPlugin);
 
-// Register route using fastify register
-server.register(app);
-// Register default fastify plugin from api
-api.register();
+// loading routes from file in routes folder
+const routes = {
+  // Loading fastify autoload
+  name: 'Autoload',
+  opts: {
+    dir: `${dirname}/routes`,
+    options: {
+      //prefix: "v1"
+    }
+  }
+};
+api.register(routes);
+
+//You can also load plugin with autoload plugin
+const plugins = {
+  // Loading fastify autoload
+  name: 'Autoload',
+  opts: {
+    dir: `${dirname}/plugins`,
+    options: {
+      //prefix: "v1"
+    }
+  }
+};
+api.register(plugins);
 
 //Start api
 api.start().then(r => {
-  api.logger.info('api started');
   // Load events
   api.events.loadEvents();
   // Call event from api
   api.events.eventEmitter.emit('start');
   api.events.eventEmitter.emit('defaultPluginRegistered');
   api.events.eventEmitter.emit('hello');
+
   // Add event after original event were loaded
   api.events.addListener({
     name: 'afterLoad',
-    log: false,
     cb: (data: any) => {
       api.logger.info('Event after loadEvent');
       api.logger.info(data);
@@ -53,4 +76,7 @@ api.start().then(r => {
   });
   // Call event from api
   api.events.eventEmitter.emit('afterLoad', { data: 'object to transfer' });
+
+  //Remove event
+  api.events.off('afterLoad');
 });
